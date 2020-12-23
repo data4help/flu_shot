9ba5d5303b190515c93282a66a3729b574affcbc
DEPMORA1 <paul.mora@de.ey.com>
2020-12-23 19:34:07 +0100

    prediction_pipeline

--- 01_code/03_pipeline_predictions.py
+++ 01_code/03_pipeline_predictions.py
@@ -5,11 +5,29 @@
 
 # Basics
 import pandas as pd
+import os
+import numpy as np
 import sys
+import copy
+from tqdm import tqdm
+import pickle
+import math
+
+# Plotting
+import matplotlib.pyplot as plt
 
 # Pipelines
 from sklearn.pipeline import make_pipeline
 from sklearn.base import TransformerMixin
+
+# Feature Importance
+from sklearn.ensemble import GradientBoostingClassifier
+
+# Predictions
+from sklearn.model_selection import GridSearchCV
+
+# Upsampling
+import imblearn
 
 # Helper Functions
 sys.path.insert(1, r'C:/Users/DEPMORA1/Documents/Projects/flu_shot/01_code')
@@ -20,8 +38,6 @@ MAIN_PATH = r'C:/Users/DEPMORA1/Documents/Projects/flu_shot'
 RAW_PATH = rf'{MAIN_PATH}/00_raw'
 DATA_PATH = rf'{MAIN_PATH}/02_data'
 OUTPUT_PATH = rf'{MAIN_PATH}/03_output'
-
-# %% Pathing
 
 # %% Data
 
@@ -38,46 +54,100 @@ train_labels = train_labels.loc[:1_000, :]
 
 # %% Feature Selection
 
+
 class FeatureSelection(TransformerMixin):
 
-    def __init__(self):
-        pass
+    def __init__(self, cutoff_threshold):
+        self.cutoff_threshold = cutoff_threshold
+
+    def plotting_feature_importance(self, df_sorted_features, target_name):
+        fig, axs = plt.subplots(figsize=(10, 10))
+        df_sorted_features.plot.bar(ax=axs)
+        path = rf'{OUTPUT_PATH}/feature_importance_{target_name}.png'
+        fig.savefig(path, bbox_inches='tight')
 
     def fit(self, X_data, y_data=None):
-        pass
+
+        # Feature Importance
+        target_name = y_data.name
+        gbm_clf = GradientBoostingClassifier()
+
+        clf = gbm_clf.fit(X_data, y_data)
+        feature_importance = clf.feature_importances_
+
+        reshaped_feature_importance = np.reshape(feature_importance, (-1, 1))
+        data_columns = X_data.columns
+        df_feature_importance = pd.DataFrame(reshaped_feature_importance, index=data_columns, columns=['columns'])
+        df_above_one_percent = df_feature_importance.query('columns >= @self.cutoff_threshold')
+        sorted_features = df_above_one_percent.sort_values(by='columns', ascending=False)
+        self.relevant_columns = list(sorted_features.index)
+        self.plotting_feature_importance(sorted_features, target_name)
+
+        return self
+
+    def transform(self, X_data, y_data=None):
+        df_relevant_columns = X_data.loc[:, self.relevant_columns]
+        return df_relevant_columns
+
+
+# %% Hyper-Parameter Tuning
+
+
+class FinalPrediction(TransformerMixin):
+
+    def __init__(self, model_parameters):
+        self.model_parameters = model_parameters
+
+    def fit(self, X_data, y_data=None):
+
+        # Adding Balancement
+        num_target_values = y_data.value_counts()
+        current_balance = math.ceil(min(num_target_values) / max(num_target_values) * 100) / 100
+        smote_grid_params = {'smote__sampling_strategy': np.linspace(current_balance, 1, num=3)}
+        grid_params = {**self.model_parameters, **smote_grid_params}
+
+        over_sampler = imblearn.over_sampling.SMOTE(random_state=28)
+        gb_model = GradientBoostingClassifier(random_state=28)
+        gb_pipeline = imblearn.pipeline.make_pipeline(over_sampler, gb_model)
+        gb_gs_clf = GridSearchCV(gb_model, grid_params, n_jobs=-1, cv=5, scoring='roc_auc')
 
     def transform(self, X_data, y_data=None):
         pass
 
 
+# %%
 
-# # %% Feature Selection
-#
-# """From the """
-#
-# # Feature Importance
-# gbm_clf = GradientBoostingClassifier()
-# dict_relevant_data = {}
-# THRESHOLD_IMPORTANCE = 1 / 100
-#
-# fig, axs = plt.subplots(figsize=(20, 10), ncols=num_target_columns)
-# axs = axs.ravel()
-# for i, y_column in enumerate(target_columns):
-#     y_data = total_train.loc[:, y_column]
-#     X_data = dict_processed_data[y_column]
-#
-#     clf = gbm_clf.fit(X_data, y_data)
-#     feature_importance = clf.feature_importances_
-#     df_feature_importance = pd.DataFrame(np.reshape(feature_importance, (-1, 1)),
-#                                          index=X_data.columns, columns=['columns'])
-#     df_above_one_percent = df_feature_importance.query('columns >= @THRESHOLD_IMPORTANCE')
-#     sorted_features = df_above_one_percent.sort_values(by='columns', ascending=False)
-#     dict_relevant_data[y_column] = dict_processed_data[y_column].loc[:, list(sorted_features.index)]
-#
-#     sorted_features.plot.bar(ax=axs[i])
-# path = rf'{GRAPH_PATH}/feature_importance.png'
-# fig.savefig(path, bbox_inches='tight')
-#
+CUTOFF_THRESHOLD = 1 / 100
+
+gb_grid_params = {
+    'gradientboostingclassifier__learning_rate': [1 / pow(10, x) for x in range(1, 3)],
+    'gradientboostingclassifier__max_depth': np.linspace(3, 30, num=3, dtype=int),
+    'gradientboostingclassifier__min_samples_leaf': np.linspace(1, 30, num=3, dtype=int),
+    'gradientboostingclassifier__n_estimators': [5],
+}
+
+
+
+over_sampler = imblearn.over_sampling.SMOTE(random_state=28)
+gb_model = GradientBoostingClassifier(random_state=28)
+gb_pipeline = imblearn.pipeline.make_pipeline(over_sampler, gb_model)
+
+
+
+predicting_pipe = make_pipeline(
+    FeatureSelection(CUTOFF_THRESHOLD),
+    FinalPrediction(gb_grid_params)
+)
+
+for target in tqdm(target_columns):
+    y_series = train_labels.loc[:, target]
+    train_features_copy = data_dict[target]['train']
+
+    predicting_instance = copy.deepcopy(predicting_pipe)
+    predicting_instance.fit(train_features_copy, y_series)
+    predicting_instance.transform(train_features_copy)
+
+
 # # %% Hyper-Parameter Tuning
 #
 # """Now we tune the hyper-parameter of the Gradient Boosting. Furthermore, we apply a SMOTE algorithm to up-sample
@@ -85,17 +155,6 @@ class FeatureSelection(TransformerMixin):
 # We try out three different levels for the SMOTE algorithm, either the status quo, midway sampling or exactly the same
 # amount.
 # """
-#
-# gb_grid_params = {
-#     'gradientboostingclassifier__learning_rate': [1 / pow(10, x) for x in range(1, 3)],
-#     'gradientboostingclassifier__max_depth': np.linspace(3, 30, num=3, dtype=int),
-#     'gradientboostingclassifier__min_samples_leaf': np.linspace(1, 30, num=3, dtype=int),
-#     'gradientboostingclassifier__n_estimators': [5],
-# }
-#
-# over_sampler = imblearn.over_sampling.SMOTE(random_state=28)
-# gb_model = GradientBoostingClassifier(random_state=28)
-# gb_pipeline = imblearn.pipeline.make_pipeline(over_sampler, gb_model)
 #
 # dict_gridsearch_results = {}
 # for y_column in tqdm(target_columns):
